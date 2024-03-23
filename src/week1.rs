c616f3f5d2846f8d8ed2fd6cf9a5d25961fe4923
chelsea <chelseawen03@gmail.com>
2024-03-23 13:40:55 -0700

    draft finished, untested code

--- src/week1.rs
+++ src/week1.rs
@@ -1,94 +1,102 @@
-/* 
+use vadeen_osm::geo::Coordinate;
+/*
 Implement a simple class RoadNetwork for an undirected graph with arc costs.
 Add a method readFromOsmFile to your class that reads an OSM file (in XML format) and
 constructs the corresponding road network. Translate the road types to speeds.
 Read the OSM files for Saarland and Baden-Wurttemberg.
 Add a line to the table linked to from the course Wiki, stating your name, the number of arcs
-and nodes in your graphs, and any other information asked for there. 
+and nodes in your graphs, and any other information asked for there.
 */
-
-
-
-use vadeen_osm::osm_io::{read, error::Error as osm_err};
+use vadeen_osm::osm_io::{error::Error as osm_err, read};
 use vadeen_osm::*;
+use std::time::Instant;
+
+#[derive(Debug, PartialEq)]
 pub struct RoadNetwork {
-    data: Osm,
     nodes: Vec<Node>,
-    edges: Vec<Edge>,
+    outgoing_arcs: Vec<Vec<Edge>>,
 }
 
-pub enum Speed {
-    motorway,
-    trunk,
-    primary,
-    secondary,
-    tertiary,
-    motorway_link, 
-    trunk_link,
-    primary_link,
-    secondary_link,
-    road,
-    unclassified,
-    residential,
-    unsurfaced,   
-    living_street,
-    service,
-}
-
-
+#[derive(Debug, PartialEq)]
 pub struct Edge {
-    head: Node,
+    head: Node,  //destination
     cost: f64,
 }
 
-pub fn cost_calc(distance: f64, road_type: Speed) -> f64 { //path: Way
-    //let road_type = path.meta.tags; //tag k = highway, v = ......
-    let kmh= match(road_type) {
-        Speed::motorway => 110,
-        Speed::trunk => 110,
-        Speed::primary=> 70,
-        Speed::secondary=> 60,
-        Speed::tertiary=> 50,
-        Speed::motorway_link=> 50, 
-        Speed::trunk_link=> 50,
-        Speed::primary_link=> 50,
-        Speed::secondary_link=> 50,
-        Speed::road=> 40,
-        Speed::unclassified=> 40,
-        Speed::residential=>30,
-        Speed::unsurfaced=>30,   
-        Speed::living_street=>10,
-        Speed::service  => 5,
+pub fn cost_calc(head: Coordinate, tail: Coordinate, way_tags: Vec<Tag>) -> f64 {
+    let road_type: &str = &(way_tags.into_iter().find(|k| k.key.eq(&"highway")).unwrap().value);
+    let kmh = match road_type {
+        "motorway" => 110,
+        "trunk" => 110,
+        "primary" => 70,
+        "secondary" => 60,
+        "tertiary" => 50,
+        "motorway_link" => 50, 
+        "trunk_link" => 50,
+        "primary_link" => 50,
+        "secondary_link" => 50,
+        "road" => 40,
+        "unclassified" => 40,
+        "residential" =>30,
+        "unsurfaced" =>30,   
+        "living_street" =>10,
+        "service" => 5,
+        _ => 0
     };
+    let distance = f64::sqrt(f64::powi(head.lat() - tail.lat(), 2) 
+                            + f64::powi(head.lon() - tail.lon(), 2));
     distance / (kmh as f64)
 }
 
-
 impl RoadNetwork {
     pub fn new(path: &str) -> Self {
-        let osm = Self::read_from_osm_file(path);
+        let osm: Osm = Self::read_from_osm_file(path).unwrap();
+        let now = Instant::now();
+        let nodes = osm.nodes;
+        let ways = osm.ways;
+        let mut outgoing_arcs: Vec<Vec<Edge>> = Vec::new();
+        for n in 0..nodes.len() {
+            let adjacent_edges: Vec<Edge> = ways.iter().map(|way| {
+                let pos = way.refs.iter().position(|&m| m == nodes[n].id);
+                if pos.is_some() {
+                    let index = way.refs[pos.unwrap() + 1];
+                    let head = nodes.iter().find(|node: &&Node| node.id == index).unwrap().clone();
+                    let cost =  cost_calc(head.coordinate, nodes[n].coordinate, way.meta.tags.clone());
+                    Some(Edge {head, cost})
+                }
+                else {None}
+                
+            }.unwrap()).collect();
+            outgoing_arcs.push(adjacent_edges);
+        }
+        println!("Construct time: {}", now.elapsed().as_secs());
         RoadNetwork {
-            data, //osm results
-            nodes, //osm.nodes
-            edges, //osm.ways transformed into the edge thing i just wrote up there it should get the head node (the destination) and the costs caluclatd using cost_calc 
+            nodes,
+            outgoing_arcs,
+            //osm.ways transformed into the edge thing i just wrote up there it should get the head node (the destination) and the costs caluclatd using cost_calc
             //need to figure out where to get edclid distance from like start node to this one???
         }
+        
     }
     pub fn read_from_osm_file(path: &str) -> Result<Osm, osm_err> {
+        let now = Instant::now();
         let osm = read(path)?;
+        println!("Reading time {}", now.elapsed().as_secs());
         Ok(osm)
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::RoadNetwork;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn run_the_thing() {
+        let result= RoadNetwork::new("saarland.osm");
+        print!("#nodes: {}, #arcs: {}", result.nodes.len(), result.outgoing_arcs.len());
     }
 }
 
-fn main(){
-
+fn main() {
+    
 }
