a00d6fc018dba12f0af3104d2b1cb512a097ae69
chelsea <chelseawen03@gmail.com>
2024-03-25 23:22:22 -0700

    i did the distance math really wrong somehow

--- src/week1.rs
+++ src/week1.rs
@@ -2,14 +2,16 @@ use osmpbfreader::objects::OsmObj;
 use std::collections::HashMap;
 
 #[derive(Debug, PartialEq, Hash, Eq, Clone, Copy, PartialOrd, Ord)]
-pub struct Node {  //nodes from OSM, each with unique ID and coordinate position
+pub struct Node {
+    //nodes from OSM, each with unique ID and coordinate position
     pub id: i64,
     pub lat: i64,
     pub lon: i64,
 }
 
 #[derive(Debug, PartialEq, Hash, Eq)]
-pub struct Way {   //ways from OSM, each with unique ID, speed from highway type, and referenced nodes that it connects
+pub struct Way {
+    //ways from OSM, each with unique ID, speed from highway type, and referenced nodes that it connects
     pub id: i64,
     pub speed: u16,
     pub refs: Vec<i64>,
@@ -21,7 +23,8 @@ pub struct RoadNetwork {
     pub edges: HashMap<i64, HashMap<i64, u16>>, // HASH < tail.id, HASH < head.id, cost >
 }
 
-fn speed_calc(highway: &str) -> Option<u16> {   //calculates speed of highway based on given values
+fn speed_calc(highway: &str) -> Option<u16> {
+    //calculates speed of highway based on given values
     match highway {
         "motorway" => Some(110),
         "trunk" => Some(110),
@@ -43,7 +46,8 @@ fn speed_calc(highway: &str) -> Option<u16> {   //calculates speed of highway ba
 }
 
 impl RoadNetwork {
-    pub fn new(nodes: HashMap<i64, Node>, ways: Vec<Way>) -> Self {     //init new RoadNetwork based on results from reading .pbf file
+    pub fn new(nodes: HashMap<i64, Node>, ways: Vec<Way>) -> Self {
+        //init new RoadNetwork based on results from reading .pbf file
         let mut edges: HashMap<i64, HashMap<i64, u16>> = HashMap::new();
         //println!("# of ways {}", ways.len());
         for way in ways {
@@ -62,9 +66,11 @@ impl RoadNetwork {
                 let head_id = way.refs[i + 1];
                 let head = nodes.get(&head_id);
                 if let (Some(tail), Some(head)) = (tail, head) {
-                    let cost = ((i64::pow((head.lat - tail.lat) * 111229, 2) as f64 / f64::powi(10.0, 4))
-                                     + (i64::pow((head.lon - tail.lon) * 71695, 2) as f64 / f64::powi(10.0, 4))
-                                    .sqrt() / (way.speed as f64)) as u16;  // km / kmph == h (hours)
+                    let cost = ((i64::pow((head.lat - tail.lat) * 111229, 2) as f64
+                        / f64::powi(10.0, 14))
+                        + (i64::pow((head.lon - tail.lon) * 71695, 2) as f64 / f64::powi(10.0, 14))
+                            .sqrt()
+                            / (1000.0 * (way.speed as f64))) as u16; // km / kmph == h (hours)
                     edges
                         .entry(tail_id)
                         .and_modify(|inner| {
@@ -93,7 +99,8 @@ impl RoadNetwork {
         Self { nodes, edges }
     }
 
-    pub fn read_from_osm_file(path: &str) -> Option<(HashMap<i64, Node>, Vec<Way>)> {       //reads osm.pbf file, values are used to make RoadNetwork
+    pub fn read_from_osm_file(path: &str) -> Option<(HashMap<i64, Node>, Vec<Way>)> {
+        //reads osm.pbf file, values are used to make RoadNetwork
         let mut nodes = HashMap::new();
         let mut ways = Vec::new();
         let path_cleaned = std::path::Path::new(&path);
@@ -128,10 +135,7 @@ impl RoadNetwork {
         Some((nodes, ways))
     }
     //TODO write this fn
-    pub fn reduce_to_largest_connected_component() {
-
-    }
-
+    pub fn reduce_to_largest_connected_component() {}
 }
 
 #[cfg(test)]
--- src/week2.rs
+++ src/week2.rs
@@ -1,9 +1,9 @@
 mod week1;
-use week1::*;
 use std::cmp::Reverse;
 use std::collections::{BinaryHeap, HashMap};
 use std::rc::Rc;
 use std::u16::MAX;
+use week1::*;
 
 /*  Add a method reduceToLargestConnectedComponent to your RoadNetwork class that reduces
         the graph (already read from an OSM file) to its largest connected component.
@@ -23,8 +23,8 @@ pub struct PathedNode {
 
 impl PathedNode {
     pub fn extract_parent<PathedNode>(last_elem: Rc<PathedNode>) -> PathedNode {
-        let inner: PathedNode = Rc::try_unwrap(last_elem)
-        .unwrap_or_else(|_| panic!("last_elem shared, unwrap failed"));
+        let inner: PathedNode =
+            Rc::try_unwrap(last_elem).unwrap_or_else(|_| panic!("last_elem shared, unwrap failed"));
         inner
     }
 }
@@ -33,13 +33,16 @@ impl PathedNode {
 impl Dijkstra {
     pub fn new(graph: RoadNetwork) -> Self {
         let mut visited_nodes = Vec::new();
-        Self {graph, visited_nodes}
+        Self {
+            graph,
+            visited_nodes,
+        }
     }
     //return node id of neighbors
-    pub fn get_neighbors (& mut self, current: Node) ->  Vec<(u16, Node)> {
+    pub fn get_neighbors(&mut self, current: Node) -> Vec<(u16, Node)> {
         let mut paths = Vec::new();
         let mut next_node_edges = HashMap::new();
-        if let Some(connections) =self.graph.edges.get(&current.id) {
+        if let Some(connections) = self.graph.edges.get(&current.id) {
             next_node_edges = connections.clone();
         }
         for path in next_node_edges {
@@ -49,8 +52,8 @@ impl Dijkstra {
     }
 
     //Uses reference to find the source node with parent_node == None
-    pub fn get_path (target: PathedNode, stored_distances: HashMap<i64, u16>) -> (Vec<Node>, u16) {
-        let mut shortest_path : Vec<Node> = Vec::new();
+    pub fn get_path(target: PathedNode, stored_distances: HashMap<i64, u16>) -> (Vec<Node>, u16) {
+        let mut shortest_path: Vec<Node> = Vec::new();
         let mut total_distance: u16 = 0;
         let mut current = target;
         while let Some(previous_node) = current.parent_node {
@@ -61,74 +64,92 @@ impl Dijkstra {
         (shortest_path, total_distance)
     }
 
-    pub fn dijkstra(& mut self, source: Node, target: Node) -> Option<(Vec<Node>, u16)> {  //set target (-1) for all-node-settle rather than just target settle or smth
+    pub fn dijkstra(&mut self, source: Node, target: Node) -> Option<(Vec<Node>, u16)> {
+        //set target (-1) for all-node-settle rather than just target settle or smth
         self.visited_nodes.push(0);
         //cost to reach nth node
         let mut stored_distance_per_node = HashMap::new();
         stored_distance_per_node.insert(source.id, 0);
-        
+
         //Heap(distance, node), Reverse turns binaryheap into minheap (default is maxheap)
         let mut priority_queue: BinaryHeap<Reverse<(u16, Node)>> = BinaryHeap::new();
         priority_queue.push(Reverse((0, source)));
-        
+
         //let _ = self.graph.nodes.iter().map(|node|{
         let graph_clone = self.graph.nodes.clone();
         for node in graph_clone {
             if !node.1.eq(&source) {
                 self.visited_nodes.push(0);
                 stored_distance_per_node.insert(node.0, MAX);
-                priority_queue.push(Reverse((MAX, node.1))); 
+                priority_queue.push(Reverse((MAX, node.1)));
             }
         }
         //});
 
-        let mut pathed_current_node: PathedNode = PathedNode { node_self: (source), parent_node: (None) };
+        let mut pathed_current_node: PathedNode = PathedNode {
+            node_self: (source),
+            parent_node: (None),
+        };
 
         while !priority_queue.is_empty() {
-            let current_node = priority_queue.pop().unwrap().0;  //.0 "unwraps" from Reverse()
-            
+            let current_node = priority_queue.pop().unwrap().0; //.0 "unwraps" from Reverse()
+
             if pathed_current_node.node_self.eq(&target) {
-                return Some(Self::get_path(pathed_current_node, stored_distance_per_node));
+                return Some(Self::get_path(
+                    pathed_current_node,
+                    stored_distance_per_node,
+                ));
             }
             let neighbors_of_current = Self::get_neighbors(self, current_node.1);
             for neighbor_node in neighbors_of_current {
-                let temp_distance = *stored_distance_per_node.get(&current_node.1.id).unwrap() + neighbor_node.0;
+                let temp_distance =
+                    *stored_distance_per_node.get(&current_node.1.id).unwrap() + neighbor_node.0;
                 if let Some(next_distance) = stored_distance_per_node.get(&neighbor_node.1.id) {
                     if temp_distance < *next_distance {
                         if let Some(x) = stored_distance_per_node.get_mut(&neighbor_node.1.id) {
                             *x = temp_distance;
                         }
                         let prev_node: Rc<PathedNode> = Rc::new(pathed_current_node);
-                        pathed_current_node = PathedNode {node_self: neighbor_node.1, parent_node: Some(Rc::clone(&prev_node))};
-                        println!("fjdkfjdkfj");
+                        pathed_current_node = PathedNode {
+                            node_self: neighbor_node.1,
+                            parent_node: Some(Rc::clone(&prev_node)),
+                        };
                         drop(prev_node);
                     }
                 }
-
             }
-            
         }
         None
-
     }
 }
 
 fn main() {}
 
-
 #[cfg(test)]
 mod tests {
     use crate::{Dijkstra, Node, RoadNetwork};
-    
+
     #[test]
     fn saarland_dijkstra() {
+        println!("starting");
         let data = RoadNetwork::read_from_osm_file("saarland_01.pbf").unwrap();
         let roads = RoadNetwork::new(data.0, data.1);
         println!("Nodes: {}, Edges: {}", roads.nodes.len(), roads.edges.len());
         let mut shortest_path_graph = Dijkstra::new(roads);
-        let source= Node {id: 314060626, lat: 491468544, lon: 71136987};
-        let target = Node {id: 314060628, lat: 491467252, lon: 71141107};
-        println!("dijiktra path and cost {:?}", shortest_path_graph.dijkstra(source, target));
+        let source = Node {
+            id: 314060626,
+            lat: 491468544,
+            lon: 71136987,
+        };
+        let target = Node {
+            id: 314060628,
+            lat: 491467252,
+            lon: 71141107,
+        };
+        println!(
+            "dijiktra path and cost {:?}",
+            shortest_path_graph.dijkstra(source, target).unwrap().1
+        );
     }
 
     /*
