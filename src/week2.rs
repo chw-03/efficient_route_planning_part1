7cd0a188ab9693fb683c6fc7e869a698f6efb817
chelsea <chelseawen03@gmail.com>
2024-03-26 19:32:46 -0700

    trying to store distance as part of pathednode

--- src/week2.rs
+++ src/week2.rs
@@ -1,8 +1,8 @@
 mod week1;
 use std::cmp::Reverse;
-use std::collections::{BinaryHeap, HashMap, VecDeque};
+use std::collections::{BinaryHeap, HashMap};
 use std::rc::Rc;
-use std::u16::MAX;
+use std::u8::MAX;
 use week1::*;
 
 /*  Add a method reduceToLargestConnectedComponent to your RoadNetwork class that reduces
@@ -14,13 +14,12 @@ use week1::*;
 pub struct Dijkstra {
     graph: RoadNetwork,
     visited_nodes: Vec<u8>,
-    stored_distance_per_node: Vec<(i64, u16)>,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone, Eq, PartialOrd, Ord)]
 pub struct PathedNode {
     node_self: Node,
-    //distance_from_start: u16
+    distance_from_start: u16,
     parent_node: Option<Rc<PathedNode>>,
 }
 
@@ -36,44 +35,40 @@ impl PathedNode {
 impl Dijkstra {
     pub fn new(graph: RoadNetwork) -> Self {
         let visited_nodes = Vec::new();
-        let stored_distance_per_node = Vec::new();
         Self {
             graph,
             visited_nodes,
-            stored_distance_per_node,
         }
     }
     //return node id of neighbors
-    pub fn get_neighbors(&mut self, current: Node) -> Vec<(u16, Node)> {
+    pub fn get_neighbors(&mut self, current: &PathedNode) -> Vec<PathedNode> {
         let mut paths = Vec::new();
         let mut next_node_edges = HashMap::new();
-        if let Some(connections) = self.graph.edges.get(&current.id) {
+        if let Some(connections) = self.graph.edges.get(&current.node_self.id) {
             next_node_edges = connections.clone();
         }
+        let current: Rc<PathedNode> = Rc::new(current.clone());
         for path in next_node_edges {
-            paths.push((path.1, *self.graph.nodes.get(&path.0).unwrap()));
+            //paths.push((path.1, *self.graph.nodes.get(&path.0).unwrap()));
+            let node_self: Node = *self.graph.nodes.get(&path.0).unwrap();
+            paths.push( PathedNode {
+                node_self: node_self,
+                distance_from_start: 0,
+                parent_node: Some(Rc::clone(&current)),
+            });
         }
         paths
     }
 
-    pub fn find_stored_distance_for_node(&mut self, current: Node) -> u16 {
-        let distance: Vec<&(i64, u16)> = self.stored_distance_per_node.iter().filter(|x| x.0 == current.id).collect();
-        distance.get(0).unwrap().1
-    }
-
-    pub fn mut_stored_distance_for_node(&mut self, current: Node) -> u16 {
-        let distance: Vec<&(i64, u16)> = self.stored_distance_per_node.iter().filter(|x| x.0 == current.id)
-    }
-
     //Uses reference to find the source node with parent_node == None
-    pub fn get_path(&mut self, target: PathedNode, stored_distances: Vec<(i64, u16)>) -> (Vec<Node>, u16) {
+    pub fn get_path(&mut self, target: PathedNode) -> (Vec<Node>, u16) {
         let mut shortest_path: Vec<Node> = Vec::new();
         let mut total_distance: u16 = 0;
         let mut current = target;
         while let Some(previous_node) = current.parent_node {
             shortest_path.push(current.node_self);
             //let prev_distance: Vec<&(i64, u16)> = stored_distances.iter().filter(|x| x.0 == current.node_self.id).collect();
-            total_distance = total_distance + Self::find_stored_distance_for_node(self, current.node_self);
+            total_distance = total_distance + current.distance_from_start;
             //total_distance = total_distance + stored_distances.get(&current.node_self.id).unwrap();
             current = PathedNode::extract_parent(previous_node);
         }
@@ -83,68 +78,71 @@ impl Dijkstra {
 
     pub fn dijkstra(&mut self, source: Node, target: Node) -> Option<(Vec<Node>, u16)> {
         //set target (-1) for all-node-settle rather than just target settle or smth
-        self.visited_nodes.push(0);
-        //cost to reach nth node
         
-        self.stored_distance_per_node.push((source.id, 0));
         //let mut stored_distance_per_node = HashMap::new();
         //stored_distance_per_node.insert(source.id, 0);
 
+        let mut source_node: PathedNode = PathedNode {
+            node_self: (source),
+            distance_from_start: 0,
+            parent_node: (None),
+        };
+
+        self.visited_nodes = (0..self.graph.nodes.len()).map(|_| MAX).collect();
+
         //Heap(distance, node), Reverse turns binaryheap into minheap (default is maxheap)
-        let mut priority_queue: BinaryHeap<Reverse<(u16, Node)>> = BinaryHeap::new();
-        priority_queue.push(Reverse((0, source)));
+        let mut priority_queue: BinaryHeap<Reverse<(u16, PathedNode)>> = BinaryHeap::new();
+        priority_queue.push(Reverse((0, source_node)));
 
         //let _ = self.graph.nodes.iter().map(|node|{
-        let graph_clone = self.graph.nodes.clone();
+        /*let graph_clone = self.graph.nodes.clone();
         for node in graph_clone {
             if !node.0.eq(&source.id) {
                 self.visited_nodes.push(0);
-                self.stored_distance_per_node.push((node.0, MAX));
                 priority_queue.push(Reverse((MAX, node.1)));
             }
-        }
+        }*/
         //});
 
-        let mut pathed_current_node: PathedNode = PathedNode {
-            node_self: (source),
-            parent_node: (None),
-        };
         let mut counter = 0;
         while !priority_queue.is_empty() {    
-            let current_node = priority_queue.pop().unwrap().0; //.0 "unwraps" from Reverse()
+            let mut pathed_current_node = priority_queue.pop().unwrap().0.1; //.0 "unwraps" from Reverse()
 
             if pathed_current_node.node_self.id.eq(&target.id) {
                 return Some(Self::get_path( self,
                     pathed_current_node,
-                    self.stored_distance_per_node,
                 ));
             }
-            let neighbors_of_current = Self::get_neighbors(self, current_node.1);
+            let neighbors_of_current = Self::get_neighbors(self, &pathed_current_node);
             
             //if counter < 3 {
                 //println!("currently at {:?} and target {:?}", pathed_current_node.node_self, target);
             //}
 
-            for neighbor_node in neighbors_of_current {
-                let temp_distance =
+            for mut neighbor_node in neighbors_of_current {
+                if priority_queue.iter().any(|member| member.0.1 == neighbor_node){
+                    priority_queue.push(Reverse((MAX.into(), neighbor_node.clone())));
+                }
+                // something to check if node was already done stuff to and skip this whole thing if yes 
+                let temp_distance = pathed_current_node.distance_from_start;
                     //*self.stored_distance_per_node.get(&current_node.1.id).unwrap() + neighbor_node.0;
-                    Self::find_stored_distance_for_node(self, current_node.1);
                 //if let Some(next_distance) = self.stored_distance_per_node.get(&neighbor_node.1.id) {
-                let next_distance = Self::find_stored_distance_for_node(self, neighbor_node.1);
+                let next_distance = neighbor_node.distance_from_start;
                     if temp_distance < next_distance {
-                        if let Some(x) = self.stored_distance_per_node.get_mut(&neighbor_node.1.id) {
-                            *x = temp_distance;
-                            let prev_node: Rc<PathedNode> = Rc::new(pathed_current_node);
+                        neighbor_node.distance_from_start = temp_distance;
+                        pathed_current_node = neighbor_node;
+                        //if let Some(x) = self.stored_distance_per_node.get_mut(&neighbor_node.1.id) {
+                            //*x = temp_distance;
+                           /* let prev_node: Rc<PathedNode> = Rc::new(pathed_current_node);
                             pathed_current_node = PathedNode {
                                 node_self: neighbor_node.1,
+                                distance_from_start: temp_distance,
                                 parent_node: Some(Rc::clone(&prev_node)),
                             };
-                            //drop(prev_node);
-                        }
+                            drop(prev_node);*/
+                        //}
                     }
-                if counter < 3 {
-                    println!("moved to node {:?}, with distance {:?}", pathed_current_node.node_self, self.stored_distance_per_node.get(&neighbor_node.1.id));
-                }
+                    
             }
             counter = counter + 1;
         }
